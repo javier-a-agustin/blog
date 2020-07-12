de634a16b7094e32fda48f7cb16bf0d99e165dcb
Javier A. Fernandez <22.javier.fernandez@gmail.com>
2020-07-12 18:50:03 -0300

    URI modificado

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+import sys
+import uri 
 #from flasktext.markdown import Markdown
 
 from datetime import datetime
@@ -11,7 +13,7 @@ app = Flask(__name__)
 #Markdown(app)
 
 # new db postgres://udharpzdqpmawc:2ff2b461f8114b3dd92347f937c680520ccbd2fe56de5f3fdf6028586c41fe15@ec2-54-236-169-55.compute-1.amazonaws.com:5432/d8971805l1evh0
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://udharpzdqpmawc:2ff2b461f8114b3dd92347f937c680520ccbd2fe56de5f3fdf6028586c41fe15@ec2-54-236-169-55.compute-1.amazonaws.com:5432/d8971805l1evh0"
+app.config['SQLALCHEMY_DATABASE_URI'] = uri.uri()
 
 db = SQLAlchemy(app)
 
